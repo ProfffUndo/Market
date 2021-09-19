3c429ba65561aed80e4ffe216c09774f5be37a6e
ProfffUndo <55656519+ProfffUndo@users.noreply.github.com>
2021-09-19 20:49:31 +0300

    Initial commit

---
+++ src/test/java/com/submarine29/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.submarine29.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
