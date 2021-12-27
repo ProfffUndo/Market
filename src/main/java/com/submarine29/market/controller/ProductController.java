c14bb2a464d9a2005767975372f1b3c799f23a21
floretn <ivanse99@yandex.ru>
2021-12-27 23:52:05 +0300

    Market-85 фильтрация с русским языком сделана

--- src/main/java/com/submarine29/market/controller/ProductController.java
+++ src/main/java/com/submarine29/market/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.submarine29.market.controller;
 
+import com.submarine29.market.domain.Category;
 import com.submarine29.market.domain.Product;
 import com.submarine29.market.dto.DataForFindDTO;
 import com.submarine29.market.services.ValidationService;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -46,6 +48,7 @@ public class ProductController {
             @RequestParam("name") Optional<String> name
             ) {
         //String category = null;
+        String str = "";
         if (from.isPresent() && to.isPresent() && from.get() > to.get()){
             model.addAttribute("priceError", "Цена от должна быть меньше цены до");
         }
@@ -71,6 +74,7 @@ public class ProductController {
                     break;
             }
         }
+        Category category = null;
         if (pageable.getPageSize() > 50 || pageable.getPageSize() < 1) {
             return "redirect:/products/?page=0&size=50";
         } else {
@@ -79,21 +83,24 @@ public class ProductController {
                         .findAllFromToPrice(pageable, from.orElse(0.00), to.orElse(Double.MAX_VALUE)));
             }
             else if (categoryId.isPresent() && name.isEmpty()){
-                //category = categoryRepo.findById(categoryId.get()).get().getName();
+                category = categoryRepo.findById(categoryId.get()).get();
                 model.addAttribute("productsPage", productRepo
                         .findAllFromToPriceAndCategory(pageable, from.orElse(0.00), to.orElse(Double.MAX_VALUE), categoryId.get()));
                 //model.addAttribute("categoryOld", category.getName());
             }
             else if (categoryId.isEmpty()){
+                str = new String(name.get().getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
                 model.addAttribute("productsPage", productRepo
-                        .findAllFromToPriceAndSearch(pageable, from.orElse(0.00), to.orElse(Double.MAX_VALUE), name.get()));
+                        .findAllFromToPriceAndSearch(pageable, from.orElse(0.00), to.orElse(Double.MAX_VALUE), str));
             }
             else {
+                category = categoryRepo.findById(categoryId.get()).get();
+                str = new String(name.get().getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
                 model.addAttribute("productsPage", productRepo
-                        .findAllFromToPriceAndCategoryAndSearch(pageable, from.orElse(0.00), to.orElse(Double.MAX_VALUE), categoryId.get(),name.get()));
+                        .findAllFromToPriceAndCategoryAndSearch(pageable, from.orElse(0.00), to.orElse(Double.MAX_VALUE), categoryId.get(), str));
             }
         }
-        DataForFindDTO dto = new DataForFindDTO(name.orElse(""), from.orElse(null), to.orElse(null), categoryId.orElse(null), sort.orElse(null));
+        DataForFindDTO dto = new DataForFindDTO(str, from.orElse(null), to.orElse(null), categoryId.orElse(null), sort.orElse(null), category);
         model.addAttribute("categories", categoryRepo.findAll());
         model.addAttribute("dto", dto);
         String params = "";
@@ -108,7 +115,7 @@ public class ProductController {
     }
 
     @PostMapping("find/find/find")
-    public String productsFind(                               @ModelAttribute("name") String name,
+    public String productsFind(@ModelAttribute("name") String name,
                                @ModelAttribute("priceFrom") String priceFrom,
                                @ModelAttribute("priceTo") String priceTo,
                                @ModelAttribute("category") String category,
@@ -118,6 +125,7 @@ public class ProductController {
         Double priceToDouble = null;
         //categoryId = categoryRepo.findByName(category).getId();
         if (!Objects.equals(category, "")){
+            category = new String(category.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
             categoryId = categoryRepo.findByName(category).getId();
         }
         if (!Objects.equals(priceFrom, "")){
@@ -128,7 +136,7 @@ public class ProductController {
         }
 
         DataForFindDTO data = new DataForFindDTO(name, priceFromDouble
-                , priceToDouble, categoryId, sort);
+                , priceToDouble, categoryId, sort, null);
         String nameFromDTO = data.getName();
 
         return "redirect:/products?" + data;
